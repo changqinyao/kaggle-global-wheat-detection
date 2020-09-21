f197391038c6332ca9cfd67f7316a0422e9762f1
‘changqinyao’ <1142253918@qq.com>
2020-09-21 18:27:05 +0800

    fcos

--- configs/fcos/fcos_center-normbbox-centeronreg-giou_r50_caffe_fpn_gn-head_4x4.py
+++ configs/fcos/fcos_center-normbbox-centeronreg-giou_r50_caffe_fpn_gn-head_4x4.py
@@ -16,6 +16,6 @@ model = dict(
 
 data = dict(samples_per_gpu=4)
 optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
-optimizer_config = dict(_delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
+optimizer_config = dict(_delete_=True, grad_clip=None)
 lr_config = dict(warmup='linear')
 load_from='/home/ubuntu/PycharmProjects/kaggle-global-wheat-detection/fcos_center-normbbox-centeronreg-giou_r50_caffe_fpn_gn-head_4x4_1x_coco_20200603-67b3859f.pth'
